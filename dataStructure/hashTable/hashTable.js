cc5d97ab6da998265ddc4914f48e748f08160d4e
mrbadri <mrbadri.dev@gmail.com>
2024-07-10 12:00:00 +0330

    feat: add delete  method for hashTable

--- dataStructure/hashTable/hashTable.js
+++ dataStructure/hashTable/hashTable.js
@@ -18,7 +18,9 @@ class HashTable {
 
   set(key, value) {
     const index = this.hash(key);
-    for (let pair of this.table[index]) {
+    const bucket = this.table[index];
+
+    for (let pair of bucket) {
       if (pair[0] === key) {
         pair[1] = value;
         return;
@@ -29,26 +31,34 @@ class HashTable {
 
   get(key) {
     const index = this.hash(key);
-    const point = this.table[index];
-    console.log("point", point);
-    for (let pair of this.table[index]) {
+    const bucket = this.table[index];
+
+    for (let pair of bucket) {
       if (pair[0] === key) {
         return pair[1];
       }
     }
     return undefined;
   }
+
+  delete(key) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        return;
+      }
+    }
+  }
 }
 
 const hashTable = new HashTable(10);
 
-// console.log(hashTable.hash("ab"));
-// console.log({ hashTable });
 hashTable.set("ab", 1234);
 hashTable.set("ba", 1234);
-console.log(hashTable[hashTable.hash("ab")]);
-hashTable.set("bac", 2111);
-hashTable.set("bac", 2111);
 
 console.log("get", hashTable.get("ab"));
-// console.log({ hashTable });
+hashTable.delete("ab");
+console.log("get", hashTable.get("ab"));
